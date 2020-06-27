f027b2a3c5576a3654fd6373ee1d8edfea32274b
IceMupppet <mabrouk.bennaoui@gmail.com>
2020-06-27 22:11:45 +0100

    replace tax with building pages

--- parsel_server.py
+++ parsel_server.py
@@ -27,7 +27,7 @@ Parcels_input=np.loadtxt('./input_parcel.csv',delimiter="\n",dtype=str)
 
 
 i=3240
-i=3761
+# i=3761
 
 len_parcel=len(Parcels_input) 
 
@@ -46,18 +46,32 @@ while i<len_parcel:
             for cookie in cookies:
                 s.cookies.set(cookie['name'], cookie['value'])
                 
-            url = "https://myplace.cuyahogacounty.us/MainPage/LegacyTaxes"
+# =============================================================================
+#             url = "https://myplace.cuyahogacounty.us/MainPage/LegacyTaxes"
+# 
+#             payload = 'hdnTaxesParcelId='+str(parcel.replace("-",""))+'&hdnTaxesListId=&hdnTaxesButtonClicked=Tax+Bill&hdnTaxesSearchChoice=Parcel&hdnTaxesSearchText='+str(parcel)+'&hdnTaxesSearchCity=99&hdnTaxYear=2019'
+#             headers = {
+#               'Content-Type': 'application/x-www-form-urlencoded'
+#             }
+#             
+#             response = s.request("POST", url, headers=headers, data = payload)
+# =============================================================================
+                
+            url = "https://myplace.cuyahogacounty.us/MainPage/PropertyData"
 
-            payload = 'hdnTaxesParcelId='+str(parcel.replace("-",""))+'&hdnTaxesListId=&hdnTaxesButtonClicked=Tax+Bill&hdnTaxesSearchChoice=Parcel&hdnTaxesSearchText='+str(parcel)+'&hdnTaxesSearchCity=99&hdnTaxYear=2019'
+            payload = 'hdnParcelId='+str(parcel.replace("-",""))+'&hdnListId=&hdnButtonClicked=Building+Information&hdnSearchChoice=Parcel&hdnSearchText='+str(parcel)+'&hdnSearchCity=99&hdnSearchPropertyNumber=&hdnSearchDeededOwner=&hdnSearchPhysicalAddress=&hdnSearchParelUnit=&hdnSearchParcelCity=&hdnSearchParcelZip=&hdnSearchPropertyType=&hdnSearchTaxLuc=&hdnSearchPropertyClass=&hdnSearchTaxLucDescription=&hdnSearchLegalDescription=&hdnSearchNeighborhoodCode='
             headers = {
-              'Content-Type': 'application/x-www-form-urlencoded'
+              'Content-Type': 'application/x-www-form-urlencoded',
             }
             
             response = s.request("POST", url, headers=headers, data = payload)
             
+            print(response.text.encode('utf8'))
+            
+            
             print("========================"+str(i))
             
-            path="./txt/demo"+str(i)+".html"
+            path="./txt_build/demo"+str(i)+".html"
             f = open(path, "w")
             f.write(str(response.text.encode('utf8')))
             f.close()
